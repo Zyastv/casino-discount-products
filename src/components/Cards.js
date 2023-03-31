70912a56a8978077a297c71db9dc372dd3ec2ca9
Zachary Adams <zach2vegas@gmail.com>
2023-03-31 14:40:16 -0700

    Tried adding an animation to the hero on load

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,41 @@
 import React from "react";
 import CardItem from "./CardItem";
 import Roulette from "./Images/IMG_0569.JPG";
+import Shoe from "./Images/IMG_0644.JPG";
+import Terms from "./Images/Terms.jpg";
+import ContactUs from "./Images/ContactUs.jpg";
+import "./Cards.css";
 
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out our different Table Acceccories!</h1>
+      <h1>Check out our Products!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
               src={Roulette}
-              text='LOREM IPSUM'
+              text='Look for Items by games!'
               label='Games'
               path='/games'
+            />
+            <CardItem
+              src={Shoe}
+              text='Look for a specific Item!'
+              label='Table Accessories'
+              path='/Table-Accessories'
+            />
+            <CardItem
+              src={ContactUs}
+              text='Let us know about any questions you have!'
+              label='Contact us'
+              path='/Contact'
+            />
+            <CardItem
+              src={Terms}
+              text='Be sure to check our terms and conditions for Freight, Sales, and Website use'
+              label='Terms and Conditions'
+              path='/Terms'
             />
           </ul>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "./Button";
 import "./Hero.css";
+import fadeIn from "react-animations/lib/fade-in";
 
 function Hero() {
   return (
